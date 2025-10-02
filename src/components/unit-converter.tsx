f8f81abd686001a729c4e2b3f5ba3c8b1df7b3a2
Ali Shahzad <alishahzadmughal2006pakistan@gmail.com>
2025-10-02 05:48:02 +0000

    Error: ./src/components/unit-converter.tsx:142:6 Parsing ecmascript sour

--- src/components/unit-converter.tsx
+++ src/components/unit-converter.tsx
@@ -95,7 +95,7 @@ const UnitConverter = () => {
             else convertedValue = numValue;
         } else if (from === 'kelvin') {
             if (to === 'celsius') convertedValue = numValue - 273.15;
-            else if (to === 'fahrenheit') convertedValue = ((fromValue - 273.15) * 9/5) + 32;
+            else if (to === 'fahrenheit') convertedValue = ((fromUnit as any - 273.15) * 9/5) + 32;
             else convertedValue = numValue;
         }
     } else {
